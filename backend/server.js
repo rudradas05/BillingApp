cc2ebd05e958c00a818eafb22cc1a6e0420fd11b
Rudra Das <154529423+rudradas05@users.noreply.github.com>
2024-12-22 18:17:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import userRouter from "./routes/userRoutes.js";
 const app = express();
 const port = process.env.PORT || 4000;
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "https://billing-4va367tkw-rudra-das-projects-91aaaad8.vercel.app/"];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "https://billing-4va367tkw-rudra-das-projects-91aaaad8.vercel.app"];
 
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.json());
